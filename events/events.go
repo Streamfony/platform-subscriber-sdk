b584b2ab219b972b7688572f105fa82deefc5201
Elijah Zobenko <ilya@zobenko.ru>
2025-05-15 03:47:53 +0300

    Add user_id to events

--- events/events.go
+++ events/events.go
@@ -3,6 +3,7 @@ package events
 type Event struct {
 	Platform string         `json:"platform"`
 	Event    string         `json:"event"`
+	UserID   uint64         `json:"user_id"`
 	Data     map[string]any `json:"data"`
 }
 
@@ -11,18 +12,20 @@ const (
 	disconnected = "disconnected"
 )
 
-func Connected(platform string) Event {
+func Connected(platform string, userID uint64) Event {
 	return Event{
 		Platform: platform,
 		Event:    connected,
 		Data:     map[string]any{},
+		UserID:   userID,
 	}
 }
 
-func Disconnected(platform string) Event {
+func Disconnected(platform string, userID uint64) Event {
 	return Event{
 		Platform: platform,
 		Event:    disconnected,
 		Data:     map[string]any{},
+		UserID:   userID,
 	}
 }
