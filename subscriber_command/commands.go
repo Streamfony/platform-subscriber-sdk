b7f090ad590e95458bbb028b0b72a940a4dfddfa
Elijah Zobenko <ilya@zobenko.ru>
2025-06-07 03:16:45 +0300

    Add orchestrator commands

---
+++ subscriber_command/commands.go
@@ -0,0 +1,49 @@
+package subscriber_command
+
+type Action string
+
+const (
+	ActionSubscribe   Action = "subscribe"
+	ActionUnsubscribe Action = "unsubscribe"
+)
+
+type Command struct {
+	Platform     string `json:"platform"`
+	PlatformType string `json:"platform_type"`
+
+	Action         Action  `json:"action"`
+	UserID         uint64  `json:"user_id"`
+	AdditionalInfo *string `json:"additional_info"`
+}
+
+type CommandFactory struct {
+	platform     string
+	platformType string
+}
+
+func NewFactory(platform, platformType string) *CommandFactory {
+	return &CommandFactory{
+		platform:     platform,
+		platformType: platformType,
+	}
+}
+
+func (c *CommandFactory) SubscribeCommand(userID uint64, additionalInfo *string) Command {
+	return Command{
+		Platform:       c.platform,
+		PlatformType:   c.platformType,
+		Action:         ActionSubscribe,
+		UserID:         userID,
+		AdditionalInfo: additionalInfo,
+	}
+}
+
+func (c *CommandFactory) UnsubscribeCommand(userID uint64, additionalInfo *string) Command {
+	return Command{
+		Platform:       c.platform,
+		PlatformType:   c.platformType,
+		Action:         ActionUnsubscribe,
+		UserID:         userID,
+		AdditionalInfo: additionalInfo,
+	}
+}
